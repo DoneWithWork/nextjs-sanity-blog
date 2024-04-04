59009ba96dcdcf42ad272d2ed517a1d8150f37c3
donewithwork <donewithworkyt@gmail.com>
2024-04-04 19:00:01 +0800

    added revalidate

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-4xl mx-auto px-4 py-5 ">
       <Link href={"/"} className="font-bold text-3xl">
-        Joshua's <span className="text-blue-500">Blog</span>
+        Joshua&apos;s <span className="text-blue-500">Blog</span>
       </Link>
       <ModeToggle />
     </nav>
